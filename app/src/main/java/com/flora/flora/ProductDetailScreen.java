72c5ebc529f42d2fd8d5d15f31c9b8c9a8d2651e
nisargpatel1504 <nisargpatel1504@gmail.com>
2021-12-14 21:25:30 -0500

    - Add ProductDetailScreen and on backpressed action -add connection between product and ProductDetailScreen -create ProductDetailScreen Layout -create red_rounded_bg.xml file for red corner background

--- app/src/main/java/com/flora/flora/ProductDetailScreen.java
+++ app/src/main/java/com/flora/flora/ProductDetailScreen.java
@@ -1,16 +1,44 @@
 package com.flora.flora;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ProductDetailScreen extends AppCompatActivity {
 
 
-    //It is just an empty activity
+    Toolbar toolbar;
+    ImageButton backImageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail_screen);
+        ProductDetailScreen.this.setTitle("");
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        findId();
+
+        backImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    public void findId(){
+        backImageButton = findViewById(R.id.back_button);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
