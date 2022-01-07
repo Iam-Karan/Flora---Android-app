e77e8b4a5150260c499de7d2ea3636d2053fdc0d
GaganKKaur <gaganm454@gmail.com>
2022-01-07 08:44:25 -0500

    - create and Design Shipin Screen - Connect it with Cart activity - add back button action - Edit Cart Screen

--- app/src/main/java/com/flora/flora/ShipingActivity.java
+++ app/src/main/java/com/flora/flora/ShipingActivity.java
@@ -1,7 +1,6 @@
 package com.flora.flora;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
@@ -9,44 +8,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class CartScreen extends AppCompatActivity {
-
+public class ShipingActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     ImageButton backImageButton;
-    AppCompatButton checkOutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart_screen);
-        CartScreen.this.setTitle("");
+        setContentView(R.layout.activity_shiping);
+        ShipingActivity.this.setTitle("");
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         findId();
-
         backImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
-        checkOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent  = new Intent(getBaseContext(), ShipingActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     public void findId(){
         backImageButton = findViewById(R.id.back_button);
-        checkOutButton = findViewById(R.id.checkout_btn);
     }
 
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, CartScreen.class);
         startActivity(intent);
     }
 }
