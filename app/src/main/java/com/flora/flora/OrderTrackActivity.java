e94ce67a11d4b247602788277a8cc43e32415d45
nisargpatel1504 <nisargpatel1504@gmail.com>
2022-01-07 10:24:30 -0500

    - create and Design Order Track Screen - Connect it with Order activity - add back button action

--- app/src/main/java/com/flora/flora/OrderTrackActivity.java
+++ app/src/main/java/com/flora/flora/OrderTrackActivity.java
@@ -2,52 +2,39 @@ package com.flora.flora;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class OrdersActivity extends AppCompatActivity {
+public class OrderTrackActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     ImageButton backImageButton;
-    CardView orderCard;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_orders);
-        OrdersActivity.this.setTitle("");
+        setContentView(R.layout.activity_order_track);
+        OrderTrackActivity.this.setTitle("");
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         findId();
-
         backImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
-
-        orderCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), OrderTrackActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     public void findId(){
         backImageButton = findViewById(R.id.back_button);
-        orderCard = findViewById(R.id.order_id);
     }
 
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, OrdersActivity.class);
         startActivity(intent);
     }
 }
